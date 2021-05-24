b2884d209aac3e404593381d4459cd8fce657d9b
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2021-05-24 15:13:03 -0500

    pseudo code for inputing user's filters

--- web_scraper/pageScraper.js
+++ web_scraper/pageScraper.js
@@ -4,6 +4,19 @@ const scraperObject = {                         // Create an object literal
         let page = await browser.newPage();     // Creates a new page instance in the browser
         console.log(`Navigating to ${this.url}...`);
         await page.goto(this.url);              // Navigate to URL
+        
+        // Check page is rendered, 'div class=main-content-container' must render
+        // If not, scroll to render
+
+        // Input user's city into placeholder of <input class="form-control>
+        // Loop through <div class="suggestion-results">
+        // Match <a class="suggestion city"></a> with users location
+        // Click matching <a class="suggestion city"></a>
+
+        // In <div class="filter-container hike">
+        // Click <button>Difficulty</button>
+        // Uncheck all levelss but users level preference
+        // Repeat process with distance button
     }
 }
 
